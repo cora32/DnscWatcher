3077545ddd459f634ccc2a28a88389920eb1676a
cora32 <cora32@tuta.io>
2017-12-24 16:38:42 +0300

    Removed unused annotation.

--- app/src/main/java/org/iskopasi/noname/repo/DnsDb.kt
+++ app/src/main/java/org/iskopasi/noname/repo/DnsDb.kt
@@ -2,12 +2,10 @@ package org.iskopasi.noname.repo
 
 import org.iskopasi.noname.App
 import org.iskopasi.noname.entities.DnscItem
-import javax.inject.Singleton
 
 /**
  * Created by cora32 on 23.12.2017.
  */
-@Singleton
 class DnsDb {
     fun getData() = App.db.dnsDao().getData()
     fun saveData(list: List<DnscItem>) = App.db.dnsDao().saveData(list)
