1ccda041e0e0436f7aa254ee9cb78b8c627fddb0
cora32 <cora32@tuta.io>
2017-12-29 00:16:38 +0300

    Watchdog script fix.

--- app/nnm.sh
+++ app/nnm.sh
@@ -5,23 +5,29 @@ do
     dnsmasq_pid="$(pidof dnsmasq)"
     dnsc_pid="$(pidof dnscrypt-proxy)"
     echo "Dnsmasq pid: $dnsmasq_pid; Dnscrypt pid: $dnsc_pid"
+    response=""
 
-    response="$(dig +short dobrochan.org)"
-    echo "Dig response: $response"
+    if [[ ( -n $dnsmasq_pid ) && ( -n $dnsc_pid ) ]]
+    then
+        response="$(dig +short dobrochan.org)"
+        echo "Dig response: $response"
+    fi
 
     if [[ -z $response ]]
     then
-            key="Your api key here"
-            data='{
-                "to":"Device token here",
-                "priority":"high",
-                "notification": {
-                    "body":"Dns failure!",
-                    "title":"Restart of Dnsmasq is required.",
-                    "sound":"default"
-                }
-    }'
-            curl -H "Authorization:key=$key" -H "Content-Type: application/json" --data "$data" https://fcm.googleapis.com/fcm/send --silent
+        echo "Dns failure!"
+        key="Your api key here"
+        data='{
+            "to":"Device token here",
+            "priority":"high",
+            "notification": {
+                "body":"Dns failure!",
+                "title":"Restart of Dnsmasq is required.",
+                "sound":"default"
+            }
+        }'
+        curl -H "Authorization:key=$key" -H "Content-Type: application/json" --data "$data" https://fcm.googleapis.com/fcm/send --silent
+    else
     fi
     sleep $((10 * 60))
 done
